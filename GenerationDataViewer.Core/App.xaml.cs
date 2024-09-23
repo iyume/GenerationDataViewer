88fdd2bacc6547d16e01deac211f7129765e32c4
iyume <iyumelive@gmail.com>
2024-09-23 17:24:44 +0800

    Initialize WPF-UI

---
+++ GenerationDataViewer.Core/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GenerationDataViewer.Core;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ GenerationDataViewer.Core/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+
+namespace GenerationDataViewer.Core;
+// Keep same as <Window x:Class="GenerationDataViewer.Core.MainWindow">
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ApplicationThemeManager.Apply(this);
+    }
+}
