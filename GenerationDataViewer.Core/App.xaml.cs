d0251280ff77009c7b0566a58ba9da51847be701
iyume <iyumelive@gmail.com>
2024-09-24 17:17:09 +0800

    add image upload and preview

--- GenerationDataViewer.Core/App.xaml.cs
+++ GenerationDataViewer.Core/App.xaml.cs
@@ -1,12 +1,15 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace GenerationDataViewer.Core;
 
 /// <summary>
-/// Interaction logic for App.xaml
+///     Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App
 {
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        // Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+        // Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+    }
 }
--- GenerationDataViewer.Core/MainWindow.xaml.cs
+++ GenerationDataViewer.Core/MainWindow.xaml.cs
@@ -1,20 +1,14 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 using Wpf.Ui.Appearance;
 
 namespace GenerationDataViewer.Core;
 // Keep same as <Window x:Class="GenerationDataViewer.Core.MainWindow">
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow
 {
@@ -22,5 +16,60 @@ public partial class MainWindow
     {
         InitializeComponent();
         ApplicationThemeManager.Apply(this);
+    }
+
+    private void UploadButton_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new();
+        openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+
+        if (openFileDialog.ShowDialog() == true)
+        {
+            UploadImageInformation.Foreground = null;
+            UploadImageInformation.Text = "Uploading image...";
+            // try?
+            BitmapImage bitmap = new();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(openFileDialog.FileName);
+            bitmap.EndInit();
+            UploadedImage.Height = 200;
+            UploadedImage.Source = bitmap;
+            UploadImageInformation.Foreground = Brushes.Green;
+            UploadImageInformation.Text = "Image uploaded";
+        }
+    }
+
+    private void UploadUrlButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (UploadImageUrl.Text.Length == 0 ||
+            (!UploadImageUrl.Text.StartsWith("http://") && !UploadImageUrl.Text.StartsWith("https://")))
+        {
+            UploadImageInformation.Foreground = Brushes.Red;
+            UploadImageInformation.Text = "Please provide a valid URL";
+            return;
+        }
+
+        UploadImageInformation.Foreground = Brushes.Black;
+        UploadImageInformation.Text = "Uploading image...";
+        BitmapImage bitmap = new();
+        bitmap.DownloadCompleted += (s, e) =>
+        {
+            UploadImageInformation.Foreground = Brushes.Green;
+            UploadImageInformation.Text = "Image uploaded";
+        };
+
+        bitmap.BeginInit();
+        try
+        {
+            bitmap.UriSource = new Uri(UploadImageUrl.Text, UriKind.Absolute);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+        bitmap.EndInit();
+        UploadedImage.Height = 200;
+        UploadedImage.Source = bitmap;
     }
 }
